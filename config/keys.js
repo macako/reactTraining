64a7ea398f632fba0d0ea6c095296e988ddbc334
Hector Olea (Contractor) <x237933@LUV.AD.SWACORP.COM>
2018-02-15 15:36:05 -0600

    Auth secction finished 2

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 //keys.js -figure out what set of credentials to return
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
