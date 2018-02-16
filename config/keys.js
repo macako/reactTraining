951294edc3b69947f896e13cc621296251645ebb
Hector Olea (Contractor) <x237933@LUV.AD.SWACORP.COM>
2018-02-15 18:46:36 -0600

    adding react application

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 //keys.js -figure out what set of credentials to return
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
