4e5af416fe9421cb53a9ab359209769e62cae4a7
Hector Olea (Contractor) <x237933@LUV.AD.SWACORP.COM>
2018-02-22 17:03:37 -0600

    adding and displaying credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,10 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="2">
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits || 0}
+          </li>,
+          <li key="3">
             <a href="/api/logout">Logout</a>
           </li>
         ];
