2cfaa4202478c8e5ef476b8adef45a7ddace7c44
macako <imacako@gmail.com>
2020-05-12 16:47:25 -0500

    Displaying Survey List

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -10,10 +10,10 @@ module.exports = survey => {
           <p>${survey.body}</p>
         </div>
         <div style="text-align: center;">
-          <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+          <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
         </div>
         <div style="text-align: center;">
-          <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+          <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
         </div>
       </body>
     </html>;
